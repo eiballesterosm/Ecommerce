3b3295c28f558183487c285effbf762638f0959f
ASUS <eiballesterosm@hotmail.com>
2019-08-13 23:45:56 -0500

    FEATURE: Implementación de las DataAnnotations

--- Entities/ProductType.cs
+++ Entities/ProductType.cs
@@ -1,12 +1,23 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Entities
 {
     public class ProductType
     {
         public int id { get; set; }
+
+        [Required(ErrorMessage = "El Nombre es requerido")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "La longitud del Nombre no es válido")]
         public string name { get; set; }
+
+        [Required(ErrorMessage = "La Descripción es requerida")]
+        [StringLength(500, MinimumLength = 4, ErrorMessage = "La longitud de la Descripción no es válida")]
         public string description { get; set; }
+
+        [Required(ErrorMessage = "La Categoría es requerida")]
         public int categoryId { get; set; }
+
         public string categoryName { get; set; }
     }
 }
